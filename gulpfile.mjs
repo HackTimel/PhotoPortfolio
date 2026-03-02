66d5057eba46fe04f491d6c3dfac90ccb58b9cff
Hack_Timel <theodore@penicaut.eu>
2026-03-02 12:02:19 +0200

    script pour redimensionner les photo qui marche pas

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -17,23 +17,47 @@ const generatedCssFiles = [
     `${cssOutputDir}/noscript.min.css`
 ];
 
+// --- GESTION DES IMAGES ---
+
+// 1. Nettoyage ciblé (uniquement les dossiers de destination)
 gulp.task('delete', function () {
-    return del(['images/*.*']);
+    return del(['images/fulls/*', 'images/thumbs/*']);
 });
 
-gulp.task('resize-images', function () {
-    return gulp.src('images/*.*')
+// 2. Génération des grandes images (1024px)
+gulp.task('resize-fulls', function () {
+    return gulp.src('images/*.{jpg,jpeg,png,gif,webp}', { allowEmpty: true })
         .pipe(imageResize({
             width: 1024,
             imageMagick: true
+        }).on('error', function(err) {
+            console.error('🚨 ERREUR ImageMagick (Fulls) :', err.message);
+            this.emit('end'); // Empêche gulp de crasher complètement
         }))
-        .pipe(gulp.dest('images/fulls'))
+        .pipe(gulp.dest('images/fulls'));
+});
+
+// 3. Génération des miniatures (512px)
+gulp.task('resize-thumbs', function () {
+    return gulp.src('images/*.{jpg,jpeg,png,gif,webp}', { allowEmpty: true })
         .pipe(imageResize({
             width: 512,
             imageMagick: true
+        }).on('error', function(err) {
+            console.error('🚨 ERREUR ImageMagick (Thumbs) :', err.message);
+            this.emit('end');
         }))
         .pipe(gulp.dest('images/thumbs'));
 });
+
+// 4. On groupe les redimensionnements en SÉRIE (très important pour ImageMagick)
+gulp.task('resize-images', gulp.series('resize-fulls', 'resize-thumbs'));
+
+// 5. La tâche finale pour les images (Nettoie PUIS redimensionne)
+gulp.task('resize', gulp.series('delete', 'resize-images'));
+
+
+// --- GESTION DU CSS / SCSS ---
 
 // clear previously generated css
 gulp.task('clean-css', function () {
@@ -42,12 +66,12 @@ gulp.task('clean-css', function () {
 
 // compile scss to css
 gulp.task('sass', gulp.series('clean-css', function compileSass() {
-    return gulp.src(cssSourceGlob)  // Target all .scss files
+    return gulp.src(cssSourceGlob)
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename(function (path) {
-            path.basename += '.min';  // Append .min to the output filename
+            path.basename += '.min';
         }))
-        .pipe(gulp.dest(cssOutputDir));  // Output to the CSS directory
+        .pipe(gulp.dest(cssOutputDir));
 }));
 
 // watch changes in scss files and run sass task
@@ -55,13 +79,15 @@ gulp.task('sass:watch', function () {
     gulp.watch('./assets/sass/**/*.scss', gulp.series('sass'));
 });
 
+
+// --- GESTION DU JAVASCRIPT ---
+
 // minify js
 gulp.task('minify-js', function () {
     return gulp.src('./assets/js/**/*.js')
         .pipe(filter(function (file) {
             const filePath = file.path;
             const basename = path.basename(filePath, '.js');
-            
             // Skip files that are already minified
             return !basename.endsWith('.min');
         }))
@@ -73,11 +99,11 @@ gulp.task('minify-js', function () {
         .pipe(gulp.dest('./assets/js'));
 });
 
+
+// --- TÂCHES GLOBALES ---
+
 // build task
 gulp.task('build', gulp.series('sass', 'minify-js'));
 
-// resize images
-gulp.task('resize', gulp.series('delete', 'resize-images'));
-
-// default task
+// default task (Lance le build CSS/JS, puis s'occupe des images)
 gulp.task('default', gulp.series('build', 'resize'));
